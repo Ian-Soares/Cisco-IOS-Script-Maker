7b1ddf4a465bd4f541e48d3181f5bdb0ef1279b3
Ian-Soares <80067490+Ian-Soares@users.noreply.github.com>
2021-06-03 11:42:21 -0300

    First commit

---
+++ ScriptMaker.py
@@ -0,0 +1,447 @@
+global commands_int_input, ip_selected
+commands_int_input = 0
+commands_int_range_input = 0
+
+def printCommandList():
+    print('''
+    ================================
+      Welcome to the Script Maker!
+    ================================
+    To choose your script commands, you must input your options like that: 1,2,3,6,8,10,13
+    (no use of spaces or [])
+
+    [1] Change Hostname
+
+    [2] Set a password 
+
+    [3] Set a enable secret
+
+    [4] Set a banner for the equipment
+
+    [5] Create VLANs
+
+    [6] Select a interface
+
+    [7] Select a interface range
+
+    [8] Create a SSH Remote Access
+    
+    [99] IP Routing
+
+    [100] Subinterface
+    ''')
+
+def printInterfaceCommandList():
+    print('''
+    [9] Set IP Address (for Routers or SW L3)
+
+    [10] Set a switchport mode (for Switches)
+
+    [11] Shutdown
+
+    [12] No shutdown
+    ''')
+
+def printVlanmodeInterface():
+    print('''
+    >>> In this interface(s) <<<
+
+    [14] For access mode
+
+    [15] For trunk mode
+    ''')
+
+def printIpRoutingCommands():
+    print('''
+    >>> Ip routing <<<
+
+    [16] For a Static Routing
+
+    [17] For a Default Routing
+    ''')
+
+def init_script():
+    print('enable')
+
+def finish_script():
+    print('wr')
+
+def hostname(name):
+    print('configure terminal')
+    print(f'hostname {name}')
+    print('end')
+
+def password(psswrd):
+    print('configure terminal')
+    print('line console 0')
+    print(f'password {psswrd}')
+    print('login')
+    print('service password-encryption')
+    print('end')
+
+def secret(secret):
+    print('configure terminal')
+    print(f'enable secret {secret}')
+    print('end')
+
+def banner(motd):
+    print('configure terminal')
+    print('line console 0')
+    print(f'banner motd "{motd}"')
+    print('end')
+
+def createVlans(number, name):
+    print('configure terminal')
+    for i in range(0, len(number)):
+        print(f'vlan {number[i]}')
+        print(f'name {name[i]}')
+    print('end')
+
+def ip_interface(interface,ip):
+    print('configure terminal')
+    print(f'interface {interface}')
+    print(f'ip address {ip[0]} {ip[1]}')
+    print('end')
+
+def vlan_access_interface(interface,vlan):
+    print('configure terminal')
+    print(f'interface {interface}')
+    print('switchport mode access')
+    print(f'switchport access vlan {vlan}')
+    print('end')
+
+def vlan_trunk_interface(interface,native,allowed):
+    print('configure terminal')
+    print(f'interface {interface}')
+    print('switchport mode trunk')
+    print(f'switchport trunk native vlan {native}')
+    print(f'switchport trunk allowed vlan {allowed}')
+    print('end')
+
+def shutdown_interface(interface):
+    print('configure terminal')
+    print(f'interface {interface}')
+    print('shutdown')
+    print('end')
+
+def no_shutdown_interface(interface):
+    print('configure terminal')
+    print(f'interface {interface}')
+    print('no shutdown')
+    print('end')
+
+def ip_interface_range(interface_range, ip):
+    print('configure terminal')
+    print(f'interface range {interface_range}')
+    print(f'ip address {ip[0]} {ip[1]}')
+    print('end')
+
+def vlan_access_interface_range(interface_range, vlan):
+    print('configure terminal')
+    print(f'interface range {interface_range}')
+    print('switchport mode access')
+    print(f'switchport access vlan {vlan}')
+    print('end')
+
+def vlan_trunk_interface_range(interface_range, native, allowed):
+    print('configure terminal')
+    print(f'interface range {interface_range}')
+    print('switchport mode trunk')
+    print(f'switchport trunk native vlan {native}')
+    print(f'switchport trunk allowed vlan {allowed}')
+    print('end')
+
+def shutdown_interface_range(interface_range):
+    print('configure terminal')
+    print(f'interface range {interface_range}')
+    print('shutdown')
+    print('end')
+
+def no_shutdown_interface_range(interface_range):
+    print('configure terminal')
+    print(f'interface range {interface_range}')
+    print('no shutdown')
+    print('end')
+
+def ssh_lineVty(ipDomainName, modulusKey, lineVTYrange, userInfo):
+    print('configure terminal')
+    print(f'ip domain-name {ipDomainName}')
+    print(f'crypto key generate rsa general-keys modulus {modulusKey}')
+    print(f'line vty {lineVTYrange}')
+    print(f'username {userInfo[0]} privilege {userInfo[1]} secret {userInfo[2]}')
+    print('end')
+
+def static_routing(id_mask_destiny,who_knows_this_network):
+    print('configure terminal')
+    print(f'ip route {id_mask_destiny[0]} {id_mask_destiny[1]} {who_knows_this_network}')
+    print('end')
+
+def default_routing(int_or_ip):
+    print('configure terminal')
+    print(f'ip route 0.0.0.0 0.0.0.0 {int_or_ip}')
+    print('end')
+
+def subinterface_config(subint, vlan_subint, ip_subint):
+    print('configure terminal')
+    print(f'interface {subint}')
+    print(f'encapsulation dot1q {vlan_subint}')
+    print(f'ip address {ip_subint}')
+    print('end')
+
+##############################
+#         INPUT AREA         #
+##############################
+
+while True:
+    try:
+        printCommandList()
+        commands1_input = input('>>> ').strip().split(',')
+    except:
+        print('Try again, something went wrong!')
+        print('The input must be like this: "1,2,3,5,7,8"')
+    for i in commands1_input:
+        if i == '1':
+            print('>>> Hostname configuration <<<')
+            hostname_chose = input('What is gonna be the hostname?: ').strip()
+
+        elif i == '2':
+            print('>>> Password configuration <<<')
+            password_chose = input('What password will you use?: ').strip()
+
+        elif i == '3':
+            print('>>> Secret configuration <<<')
+            secret_chose = input('What will be your secret?: ').strip()
+
+        elif i == '4':
+            print('>>> Banner configuration <<<')
+            banner_chose = input('What will be your banner?: ').strip()
+        
+        elif i == '5':
+            while True:
+                try:    
+                    print('>>> Creating VLANs <<<')
+                    vlanNumbers_chose = input('Insert the vlan numbers you will need. (example: 10,20,30,40,50,60): ').strip().split(',')
+                    vlanNames_chose = input('Insert now the name for those VLANs, in the same order (example: IT,ADMIN,STUDENTS): ').strip().split(',')
+                    if len(vlanNames_chose) == len(vlanNumbers_chose):
+                        break
+                    else:
+                        print('You need to enter the same amount of values in the VLAN numbers and VLAN names.')
+                except:
+                    print('Sorry, but the value for your VLAN number or name is not available.')
+        elif i == '6':
+            print('>>> Interface selection <<<')
+            interface_selected = input('What will be your interface selected? (example: g0/1 or g1/0/24): ').strip()
+            printInterfaceCommandList()
+            try:
+                commands_int_input = input('>>> ').strip().split(',')
+                for i in commands_int_input:
+                    if i == '9':
+
+                        while True:
+                            try:
+                                print('>>> Interface IP configuration <<<')
+                                print('What will be the IP Address and Subnet Mask?')
+                                ip_selected = input('example: 172.16.1.1,255.255.255.0): ').strip().split(',')
+                                if len(ip_selected) == 2:
+                                    break
+                                else:
+                                    print('----- You need to enter the IP Address and the Subnet Mask. -----')
+                                    print('----- Try something like this: 192.168.0.138,255,255,255,128 -----')
+                            except:
+                                print('Something went wrong, try a input like this: 10.128.0.1,255.128.0.0')
+                    
+                    elif i == '10':
+                        while True:
+                            printVlanmodeInterface()
+                            interface_vlan_mode = input('>>> ').strip()
+                            if interface_vlan_mode == '14':
+                                print('>>> Access interface configuration <<<')
+                                access_interface = input('Which will be the vlan? (example: 10): ').strip()
+                                break
+                            elif interface_vlan_mode == '15':
+                                print('>>> Trunk interface configuration <<<')
+                                trunk_native_interface = input('Insert the native vlan(example: 99): ').strip()
+                                trunk_allowed_interface = input('Insert the allowed vlans(like this: 10,20,30,99): ').strip()
+                                break
+                            else:
+                                print('Sorry, that is not an option.')
+                    
+                    elif i == '11':
+                        continue
+                    
+                    elif i == '12':
+                        continue
+                    
+                    else:
+                        print(f'Number {i} could not be reached.')
+            
+            except:
+                print('Could not understand it.')
+
+        elif i == '7':
+            print('>>> Interface range configuration <<<')
+            interface_range_selected = input('Enter your interface range selected (example: f0/0-24): ').strip()
+            printInterfaceCommandList()
+            
+            try:
+                commands_int_range_input = input('>>> ').strip().split(',')
+            
+                for i in commands_int_range_input:
+                    if i == '9':
+            
+                        while True:
+                            try:
+                                print('What will be the IP Address and Subnet Mask?')
+                                ip_range_selected = input('example: 172.16.1.1,255.255.255.0): ').strip().split(',')
+                                if len(ip_range_selected) == 2:
+                                    break
+                                else:
+                                    print('You need to enter the IP Address and the Subnet Mask.')
+                                    print('Try something like this: 192.168.0.138,255,255,255,128')
+                            except:
+                                print('Something went wrong, try a input like this: 10.128.0.1,255.128.0.0')
+                    
+                    elif i == '10':
+                        while True:
+                            printVlanmodeInterface()
+                            interface_range_vlan_mode = input('>>> ').strip()
+                            if interface_range_vlan_mode == '13':
+                                print('>>> Access interfaces configuration <<<')
+                                access_interface_range = input('Which will be the VLAN? (example: 10): ').strip()
+                                break
+                            elif interface_range_vlan_mode == '14':
+                                print('>>> Trunk interfaces configuration <<<')
+                                trunk_native_interface_range = input('Insert the native VLAN(example: 99): ').strip()
+                                trunk_allowed_interface_range = input('Insert the allowed VLANs(like this: 10,20,30,99): ').strip()
+                                break
+                            else:
+                                print('Sorry, that is not an option.')
+
+                    elif i == '11':
+                        continue
+
+                    elif i == '12':
+                        continue
+
+                    else:
+                        print(f'Number {i} could not be reached.')
+
+            except:
+                print('Could not understand it.')
+
+        elif i == '8':
+            while True:
+                try:
+                    print('>>> SSH Configuration <<<')
+                    ip_domain_name_chose = input('Enter the ip domain-name: ')
+                    encryptionModulusSSH = input('Enter the amount of bits in your key encryptation(example: 1024): ')
+                    lineVTY_chose = input('Select the line VTY range (example: 0 15): ').strip()
+                    username_priv_secret = input('Enter the username, the privilege and the secret in sequence (example: ADMIN,15,Secret*00): ').strip().split(',')
+                    break
+                except:
+                    print('Something went wrong, try again.')
+
+        elif i == '99':
+            while True:
+                try:
+                    printIpRoutingCommands()
+                    commands_ipRouting_input = input('Which command(s) do you want to define?(example: 16,17): ').strip().split(',')
+                    for i in commands_ipRouting_input:
+                        if i == '16':
+                            print('>>> Static ip routing <<<')
+                            destiny_id_and_mask = input('Enter the id and mask of the destiny network (example: 172.16.0.0,255.255.0.0): ').strip().split(',')
+                            who_knows_the_net = input('Enter the interface or next host neighbour (example: s0/0/1 or 200.100.0.1): ').strip()
+                        if i == '17':
+                            print('>>> Default ip routing <<<')
+                            default_who_knows_the_net = input('Enter the interface or next host neighbour (example: s0/0/1 or 200.100.0.1): ').strip()
+                    break
+                except:
+                    print('Something went wrong, try again.')
+
+        elif i == '100':
+            while True:
+                try:
+                    print('>>> Subinterface configuration <<<')
+                    subinterface_chose = input('Enter the subinterface (example: g0/0.10): ').strip()
+                    subinterface_vlan_chose = input('Which VLAN will this interface use?(example: 10): ').strip()
+                    subinterface_ip_chose = input('Enter the subinterface IP: ').strip()
+                    break
+                except:
+                    print('Something went wrong, try again.')
+        else:
+            print(f'The number {i} could not be reached.')
+    break
+
+##############################
+#        OUTPUT AREA         #
+##############################
+
+print('''
+Your script is:
+''')
+init_script()
+for i in commands1_input:
+    if i == '1':
+        hostname(hostname_chose)
+
+    elif i == '2':
+        password(password_chose)
+
+    elif i == '3':
+        secret(secret_chose)
+
+    elif i == '4':
+        banner(banner_chose)
+
+    elif i == '5':
+        createVlans(vlanNumbers_chose, vlanNames_chose)
+
+    elif i == '6':
+        
+        if commands_int_input != 0:
+            for i in commands_int_input:
+                if i == '9':
+                    ip_interface(interface_selected, ip_selected)
+                elif i == '10':
+                    if interface_vlan_mode == '14':
+                        vlan_access_interface(interface_selected,access_interface)
+                    elif interface_vlan_mode == '15':
+                        vlan_trunk_interface(interface_selected, trunk_native_interface, trunk_allowed_interface)
+                elif i == '11':
+                    shutdown_interface(interface_selected)
+                elif i == '12':
+                    no_shutdown_interface(interface_selected)
+
+    elif i == '7':
+        
+        if commands_int_range_input != 0:
+            for i in commands_int_range_input:
+                if i == '9':
+                    ip_interface_range(interface_range_selected, ip_range_selected)
+                elif i == '10':
+                    if interface_range_vlan_mode == '14':
+                        vlan_access_interface_range(interface_range_selected, access_interface_range)
+                    elif interface_range_vlan_mode == '15':
+                        vlan_trunk_interface_range(interface_range_selected, trunk_native_interface_range, trunk_allowed_interface_range)
+                elif i == '11':
+                    shutdown_interface_range(interface_range_selected)
+                elif i == '12':
+                    no_shutdown_interface_range(interface_range_selected)
+    
+    elif i == '8':
+        
+        ssh_lineVty(ip_domain_name_chose, encryptionModulusSSH, lineVTY_chose, username_priv_secret)
+
+    elif i == '99':
+        
+        for i in commands_ipRouting_input:
+            if i == '16':
+                static_routing(destiny_id_and_mask, who_knows_the_net)
+            elif i == '17':
+                default_routing(default_who_knows_the_net)
+
+    elif i == '100':
+        subinterface_config(subinterface_chose,subinterface_vlan_chose,subinterface_ip_chose)
+
+finish_script()
+print()
